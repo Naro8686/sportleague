fccbdd90e88c5a1278f05ea45469404522ae8c4e
armanarakelyan <armanarakelyan11@gmail.com>
2020-02-12 19:46:26 +0400

    12.02.2020 second

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,21 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <title>Reset password</title>
     @include('partials.front.head')
+    <style>
+        .has-feedback .invalid-feedback {
+            display: block;
+        }
+    </style>
 </head>
 <body>
-
-<!-- preloader area start -->
-<div class="preloader" id="preloader">
-    <div class="preloader-inner">
-        <div class="spinner">
-            <div class="dot1"></div>
-            <div class="dot2"></div>
-        </div>
-    </div>
-</div>
-<!-- preloader area end -->
 
 <div class="signinup-area">
     <div class="content col-lg-6 col-md-6">
