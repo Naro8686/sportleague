411232b5936b6d56ed2175d3a7ca3e654327dc73
Lavrenti Serov <lavrentiserov@gmail.com>
2020-03-03 18:39:24 +0400

    03.03.2020 Install error

--- app/Providers/MailConfigServiceProvider.php
+++ app/Providers/MailConfigServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Settings;
 use Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class MailConfigServiceProvider extends ServiceProvider
 {
@@ -26,22 +27,24 @@ class MailConfigServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $smtp = DB::table('settings')->where('title', 'SMTP')->first();
-        if(!is_null($smtp)){
-            $smtp = json_decode($smtp->content);
+        if(Schema::hasTable('settings')){
+            $smtp = DB::table('settings')->where('title', 'SMTP')->first();
+            if(!is_null($smtp)){
+                $smtp = json_decode($smtp->content);
 
-            $config = array(
-                'driver' => $smtp->driver,
-                'host' => $smtp->host,
-                'port' => $smtp->port,
-                'from' => array('address' => $smtp->from, 'name' => $smtp->from_name),
-                'encryption' => $smtp->encryption,
-                'username' => $smtp->username,
-                'password' => $smtp->password,
-                'sendmail' => '/usr/sbin/sendmail -bs',
-                'pretend' => false,
-            );
-            Config::set('mail', $config);
+                $config = array(
+                    'driver' => $smtp->driver,
+                    'host' => $smtp->host,
+                    'port' => $smtp->port,
+                    'from' => array('address' => $smtp->from, 'name' => $smtp->from_name),
+                    'encryption' => $smtp->encryption,
+                    'username' => $smtp->username,
+                    'password' => $smtp->password,
+                    'sendmail' => '/usr/sbin/sendmail -bs',
+                    'pretend' => false,
+                );
+                Config::set('mail', $config);
+            }
         }
     }
 }
