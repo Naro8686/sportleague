41fcf35482dc4abfde0d4020d138f9e5caa664aa
Lavrenti Serov <lavrentiserov@gmail.com>
2020-03-03 16:58:11 +0400

    03.03.2020 Reset password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -17,6 +17,13 @@
                 <a href="{{ route('home') }}"><img src="{{ asset('front-assets/img/logo-black.png') }}" alt="logo"></a>
             </div>
             <div class="panel-body">
+                @if($errors->any())
+                    <div class="alert-danger">
+                        @if($errors->any())
+                            {!! implode('', $errors->all('<div>:message</div>')) !!}
+                        @endif
+                    </div>
+                @endif
                 <form method="POST" action="{{ route('password.request') }}">
                     {{ csrf_field() }}
                     <p class="text-muted"></p>
@@ -31,13 +38,14 @@
                             @endif
                         </div>
                         <div class="form-group has-feedback">
-                            <input type="password" name="password" class="form-control" required placeholder="Password">
+                            <input type="password" id="password" name="password" class="form-control" required placeholder="Password">
                             @if($errors->has('password'))
                                 <em class="invalid-feedback">
                                     {{ $errors->first('password') }}
                                 </em>
                             @endif
                         </div>
+
                         <div class="form-group has-feedback">
                             <input type="password" name="password_confirmation" class="form-control" required placeholder="Password confirmation">
                             @if($errors->has('password_confirmation'))
@@ -46,10 +54,43 @@
                                 </em>
                             @endif
                         </div>
+
+                        <div id="popover-password">
+                            <p>Password Strength: <span id="result"> </span></p>
+                            <div class="progress" style="height: 5px;">
+                                <div id="password-strength" class="progress-bar progress-bar-success"
+                                     role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"
+                                     style="width:0%">
+                                </div>
+                            </div>
+                            <ul class="list-unstyled">
+                                <li class="">
+                                        <span class="low-upper-case">
+                                            <i class="fa fa-file-text" aria-hidden="true"></i>
+                                        </span>&nbsp; 1 lowercase &amp; 1 uppercase
+                                </li>
+                                <li class="">
+                                        <span class="one-number">
+                                            <i class="fa fa-file-text" aria-hidden="true"></i>
+                                        </span>&nbsp; 1 number (0-9)
+                                </li>
+                                <li class="">
+                                        <span class="one-special-char">
+                                            <i class="fa fa-file-text" aria-hidden="true"></i>
+                                        </span>&nbsp; 1 Special Character (!@#$%^&*).
+                                </li>
+                                <li class="">
+                                        <span class="eight-character">
+                                            <i class="fa fa-file-text" aria-hidden="true"></i>
+                                        </span>&nbsp; Atleast 8 Character
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
                     <div class="row">
                         <div class="col-12 text-right">
-                            <button type="submit" class="btn sm-btn w-100 mb-4">
+                            <button type="submit" class="btn sm-btn w-100 mb-4 reset_pass" disabled>
                                 Reset Password
                             </button>
                         </div>
@@ -61,6 +102,106 @@
 </div>
 
 @include('partials.front.js')
+<style>
+    .fa-file-text {
+        color: #000;
+    }
+
+    #popover-password, #popover-password p {
+        text-align: left;
+        color: #000;
+    }
+
+    #popover-password .progress {
+        background-color: var(--main-color);
+    }
+
+    #popover-password .progress-bar {
+        background-color: green;
+    }
+
+    #popover-password, .list-unstyled {
+        margin: 10px 0 0 0;
+    }
+</style>
+<script>
+    $(document).ready(function () {
+        $('#password').keyup(function () {
+            let password = $('#password').val();
+            if (checkStrength(password) == false) {
+                $('#sign-up').attr('disabled', true);
+            }
+        });
+
+        function checkStrength(password) {
+            let strength = 0;
+            if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+                strength += 1;
+                $('.low-upper-case').addClass('text-success');
+                $('.low-upper-case i').removeClass('fa-file-text').addClass('fa-check');
+                $('#popover-password-top').addClass('hide');
+            } else {
+                $('.low-upper-case').removeClass('text-success');
+                $('.low-upper-case i').addClass('fa-file-text').removeClass('fa-check');
+                $('#popover-password-top').removeClass('hide');
+            }
+            if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) {
+                strength += 1;
+                $('.one-number').addClass('text-success');
+                $('.one-number i').removeClass('fa-file-text').addClass('fa-check');
+                $('#popover-password-top').addClass('hide');
+            } else {
+                $('.one-number').removeClass('text-success');
+                $('.one-number i').addClass('fa-file-text').removeClass('fa-check');
+                $('#popover-password-top').removeClass('hide');
+            }
+            if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+                strength += 1;
+                $('.one-special-char').addClass('text-success');
+                $('.one-special-char i').removeClass('fa-file-text').addClass('fa-check');
+                $('#popover-password-top').addClass('hide');
+            } else {
+                $('.one-special-char').removeClass('text-success');
+                $('.one-special-char i').addClass('fa-file-text').removeClass('fa-check');
+                $('#popover-password-top').removeClass('hide');
+            }
+            if (password.length > 7) {
+                strength += 1;
+                $('.eight-character').addClass('text-success');
+                $('.eight-character i').removeClass('fa-file-text').addClass('fa-check');
+                $('#popover-password-top').addClass('hide');
+            } else {
+                $('.eight-character').removeClass('text-success');
+                $('.eight-character i').addClass('fa-file-text').removeClass('fa-check');
+                $('#popover-password-top').removeClass('hide');
+            }
+
+            if (strength < 2) {
+                $('#result').removeClass()
+                $('#password-strength').addClass('progress-bar-danger');
+
+                $('#result').addClass('text-danger').text('Very Week');
+                $('#password-strength').css('width', '10%');
+            } else if (strength == 2) {
+                $('#result').addClass('good');
+                $('#password-strength').removeClass('progress-bar-danger');
+                $('#password-strength').addClass('progress-bar-warning');
+                $('#result').addClass('text-warning').text('Week')
+                $('#password-strength').css('width', '60%');
+                return 'Week'
+            } else if (strength == 4) {
+                $('#result').removeClass()
+                $('#result').addClass('strong');
+                $('#password-strength').removeClass('progress-bar-warning');
+                $('#password-strength').addClass('progress-bar-success');
+                $('#result').addClass('text-success').text('Strength');
+                $('#password-strength').css('width', '100%');
+                $('.reset_pass').attr('disabled', false);
+                return 'Strong'
+            }
+        }
+    });
+</script>
 
 </body>
 </html>
